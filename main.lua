55a6d3279f48bf04b5a6961ed3158a023fb61241
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2025-01-25 15:02:34 -0600

    update: created a tilemap

--- main.lua
+++ main.lua
@@ -4,24 +4,53 @@ function love.load()
     Tileset = love.graphics.newImage('countryside.png')
 
     -- tell love the width and heigth for each tile
-    local tileW, tileH = 32,32
+    TileW, TileH = 32,32
 
     -- heighth and width for the whole tileset
     local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
 
-    GrassQuad = love.graphics.newQuad(0, 0, tileW, tileH, tilesetW, tilesetH)
-    BoxQuad = love.graphics.newQuad(32, 0, tileW, tileH, tilesetW, tilesetH)
+    Quads = 
+    {
+        -- grass
+        love.graphics.newQuad(0, 0, TileW, TileH, tilesetW, tilesetH),
+        -- box
+        love.graphics.newQuad(32, 0, TileW, TileH, tilesetW, tilesetH),
+        -- flower grass
+        love.graphics.newQuad(0, 32, TileW, TileH, tilesetW, tilesetH),
+        -- boxtop
+        love.graphics.newQuad(32, 32, TileW, TileH, tilesetW, tilesetH)
+    }
+
+    TileTable = 
+    {
+        { 4,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4 },
+        { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,4 },
+        { 4,1,3,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,4 },
+        { 4,1,1,1,1,1,1,1,2,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,4 },
+        { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4 },
+        { 4,1,1,4,1,1,1,1,1,2,2,1,1,4,1,1,1,4,1,4,2,2,1,1,4 },
+        { 4,1,1,4,1,1,1,1,4,3,3,4,1,2,1,1,1,2,1,4,1,1,1,1,4 },
+        { 4,1,1,4,1,1,1,1,4,3,3,4,1,1,4,1,4,1,1,4,2,2,1,1,4 },
+        { 4,1,1,4,1,1,1,1,4,3,3,4,1,1,2,1,2,1,1,4,1,1,1,1,4 },           
+        { 4,1,1,4,1,1,1,1,2,3,3,2,1,1,1,4,1,1,1,4,1,1,1,1,4 },
+        { 4,1,1,2,2,2,2,1,1,2,2,1,1,1,1,2,1,3,1,2,2,2,1,1,4 },
+        { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4 },
+        { 4,1,1,1,1,1,1,1,1,2,1,1,1,1,2,2,4,1,1,1,1,1,1,1,4 },
+        { 4,1,1,1,1,1,1,1,4,3,4,1,1,1,1,1,2,1,1,1,1,1,1,1,4 },
+        { 4,1,1,3,1,1,1,1,2,3,2,1,1,1,1,2,1,1,1,1,1,1,1,1,4 },
+        { 4,1,1,1,1,1,1,1,1,2,1,1,2,1,2,1,1,1,1,1,1,1,3,1,4 },
+        { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4 },
+        { 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2 }
+    }
 end
 
 
 function love.draw()
-    love.graphics.draw(Tileset, BoxQuad, 368, 268)
-    love.graphics.draw(Tileset, BoxQuad, 400, 268)
-    love.graphics.draw(Tileset, BoxQuad, 432, 268)
-    love.graphics.draw(Tileset, GrassQuad, 368, 300)
-    love.graphics.draw(Tileset, BoxQuad  , 400, 300)
-    love.graphics.draw(Tileset, GrassQuad, 432, 300)
-    love.graphics.draw(Tileset, GrassQuad, 368, 332)
-    love.graphics.draw(Tileset, BoxQuad, 400, 332)
-    love.graphics.draw(Tileset, GrassQuad, 432, 332)
+    for rowIdx = 1, #TileTable do
+        local row = TileTable[rowIdx]
+        for colIdx = 1, #row do
+            local number = row[colIdx]
+            love.graphics.draw(Tileset, Quads[number], (colIdx-1) * TileW, (rowIdx-1) * TileH)
+        end
+    end
 end
