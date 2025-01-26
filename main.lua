d7a9efd9bb35214c48e69f0c08f72afa122c1e9b
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2025-01-26 13:37:03 -0600

    update: partially works, but i kinda hate how maps are loaded in

--- main.lua
+++ main.lua
@@ -1,81 +1,9 @@
-function loadMap()
-    Tileset = love.graphics.newImage('countryside.png')
-
-    -- tell love the width and heigth for each tile
-    TileW, TileH = 32,32
-
-    -- heighth and width for the whole tileset
-    local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
-
-    local quadInfo = {
-        { '.',  0,  0 }, -- 1 = grass 
-        { '#', 32,  0 }, -- 2 = box
-        { '*', 0, 32 }, -- 3 = flowers
-        { '^', 32, 32 },  -- 4 = boxTop
-        { '3', 64, 0 },  -- 5 = cat
-        { 'W', 96, 0 },  -- 6 = old window
-        { 'S', 64, 32 },  -- 7 = sign in grass
-        { '>', 96, 32 },  -- 8 = red happy face
-    }
-
-    Quads = {}
-    -- ipairs does i ++ on current loop and quadinfo loop
-    for _, info in ipairs(quadInfo) do
-        -- info[i] = x, info[2] = y
-        Quads[info[1]] = love.graphics.newQuad(info[2], info[3], TileW, TileH, tilesetW, tilesetH)
-    end
-
-    local tileString = [[
-^#######################^
-^....................*..^
-^..*....................^
-^.###....#...^*^........^
-^.^..#..^.^..#.#.*..S...^
-^.^##...^.^...^.........^
-^.^..#.^###^..^..3......^
-^.###..#...#..#.....S...^
-^.......................^
-^.......***.............^
-^...*........>..........^
-^.................*.....^
-^........*..........*...^
-^.......................^
-^...***3***3**S******...^
-^................W......^
-^..*..................*.^
-#########################]]
-
-    TileTable = {}
-
-    -- parsing tiles
-    local width = #(tileString:match("[^\n]+"))
-
-    for x = 1, width, 1 do TileTable[x] = {} end
-
-    local rowIdx, colIdx = 1, 1
-
-    for row in tileString:gmatch("[^\n]+") do
-        assert(#row == width, "map is not aligned: width of row" .. tostring(rowIdx) .. ' should be ' .. tostring(width) .. ', but it is ' .. tostring(#row))
-        colIdx = 1
-        for char in row:gmatch(".") do
-            TileTable[colIdx][rowIdx] = char
-            colIdx = colIdx + 1
-        end
-        rowIdx = rowIdx + 1
-    end
-end
-
-function drawMap()
-    for columnIndex, column in ipairs(TileTable) do
-        for rowIndex, char in ipairs(column) do
-            local x,y = (columnIndex-1)*TileW, (rowIndex-1)*TileH
-            love.graphics.draw(Tileset, Quads[char], x, y)
-        end
-    end
-end
+require 'map-funtions'
 
 function love.load()
-    loadMap()
+
+    loadMap('maps/bay-hello.lua')
+    
 
 end
 
---
+++ map-funtions.lua
@@ -0,0 +1,47 @@
+function loadMap(path)
+   love.filesystem.load(path)()
+end
+
+function drawMap()
+    for columnIndex, column in ipairs(TileTable) do
+        for rowIndex, char in ipairs(column) do
+            local x,y = (columnIndex-1)*TileW, (rowIndex-1)*TileH
+            love.graphics.draw(Tileset, Quads[char], x, y)
+        end
+    end
+end
+
+
+function newMap(tileW, tileH, tilesetPath, tileString, quadInfo)
+  
+    TileW = tileW
+    TileH = tileH
+    Tileset = love.graphics.newImage(tilesetPath)
+    
+    local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
+    
+    Quads = {}
+    
+    for _,info in ipairs(quadInfo) do
+      -- info[1] = the character, info[2] = x, info[3] = y
+      Quads[info[1]] = love.graphics.newQuad(info[2], info[3], TileW,  TileH, tilesetW, tilesetH)
+    end
+    
+    TileTable = {}
+    
+    local width = #(tileString:match("[^\n]+"))
+  
+    for x = 1,width,1 do TileTable[x] = {} end
+  
+    local x,y = 1,1
+    for row in tileString:gmatch("[^\n]+") do
+      assert(#row == width, 'Map is not aligned: width of row ' .. tostring(y) .. ' should be ' .. tostring(width) .. ', but it is ' .. tostring(#row))
+      x = 1
+      for tile in row:gmatch(".") do
+        TileTable[x][y] = tile
+        x = x + 1
+      end
+      y=y+1
+    end
+  
+  end
